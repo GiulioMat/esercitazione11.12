5dd06e60d8e4beba16c4bd07e4e3f8ce259e88e2
Giulio <giulio.mattolin@studenti.unitn.it>
2018-12-11 15:26:11 +0100

    funzione finita

--- app.js
+++ app.js
@@ -8,5 +8,15 @@ app.get("/", function(req, res){
     res.send("Esercitazione 11.12.2018");
 })
 
-app.listen(PORT, () => console.log('Example app listening on port: ' + PORT));
+app.get("/square", function(req, res){
+    var stringa = req.query.string;
+    if(isNaN(stringa)){
+        var lunghezza = stringa.length;
+        var quadrato = (lunghezza*lunghezza);
+        res.json(quadrato);
+    }
+    res.json(-1);
+})
+
+app.listen(PORT, () => console.log('Example app listening on port: ' + PORT))
 
